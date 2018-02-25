2af6d0b2076a139e369f54c633cb2f1fa2c8ed61
onahump <oswaldo@makingdevs.com>
2018-02-25 12:49:00 -0600

    Adding examble db access

---
+++ Spring-DataBase/src/test/groovy/com/makingdevs/nahum/NahumApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.makingdevs.nahum
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class NahumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
