60efcb3cde5b852308909412a9649fde86e6cea0
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-26 12:51:33 -0600

    Importing some methods and changing the package name

--- DataBase-Spring/src/main/groovy/com/makingdevs/nahum/User.groovy
+++ DataBase-Spring/src/main/groovy/com/makingdevs/nahum/User.groovy
@@ -1,7 +1,12 @@
-package com.makingevs.nahum
+package com.makingdevs.nahum
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity // This tells Hibernate to make a table out of this class
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id
