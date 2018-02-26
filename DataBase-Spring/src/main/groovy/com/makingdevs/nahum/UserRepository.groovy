056e6f1726ac57314f42b2575464d3908793fd99
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-02-26 12:52:15 -0600

    Changing the package name from UserRepository

--- DataBase-Spring/src/main/groovy/com/makingdevs/nahum/UserRepository.groovy
+++ DataBase-Spring/src/main/groovy/com/makingdevs/nahum/UserRepository.groovy
@@ -2,6 +2,8 @@ package com.makingdevs.nahum
 
 import org.springframework.data.repository.CrudRepository
 
+import com.makingdevs.nahum.User
+
 public interface UserRepository extends CrudRepository<User, Long>{
 	
 }
