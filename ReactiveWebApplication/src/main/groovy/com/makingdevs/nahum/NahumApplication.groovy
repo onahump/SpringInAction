3593230a2280553ffe31b116a86b4290ce369057
Oswaldo Nahum Ponce Arriaga <oswaldo@makingdevs.com>
2018-03-15 10:48:38 -0600

    Creating my first reactive web application

---
+++ ReactiveWebApplication/src/main/groovy/com/makingdevs/nahum/NahumApplication.groovy
@@ -0,0 +1,12 @@
+package com.makingdevs.nahum
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NahumApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run NahumApplication, args
+	}
+}
